b31a1d3edfadba29e499dec1f303839ed039fec3
Alvito Novandi <alvitonovandi890@gmail.com>
2026-09-07 13:54:25 +0700

    refactor: change primary key from id to uuid in multiple migration files, update seeder data, and fix ui

--- database/migrations/2026_06_26_124749_create_perusahaan_dan_dokumen.php
+++ database/migrations/2026_06_26_124749_create_perusahaan_dan_dokumen.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('perusahaan', function (Blueprint $table) {
-            $table->id();
+            $table->uuid();
             $table->string('nama');
             $table->string('sektor')->nullable();
             $table->text('deskripsi')->nullable();
